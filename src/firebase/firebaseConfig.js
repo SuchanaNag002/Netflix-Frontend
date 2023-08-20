49544eb7c5dea8935a12d40c2caa696cce0bf80c
Suchana Nag <99524477+SuchanaNag002@users.noreply.github.com>
2023-08-20 14:06:56 +0530

    Update firebaseConfig.js

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -1,15 +1,14 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 const firebaseConfig = {
-  apiKey: "AIzaSyCDDKqJ7GW4Xy7JfVehX4cYOyMu4_1sYO4",
-  authDomain: "netflix-clone-1d3a2.firebaseapp.com",
-  projectId: "netflix-clone-1d3a2",
-  storageBucket: "netflix-clone-1d3a2.appspot.com",
-  messagingSenderId: "786631608991",
-  appId: "1:786631608991:web:c5d7d91764bc1015b170aa",
-  measurementId: "G-M0GJF84361",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
